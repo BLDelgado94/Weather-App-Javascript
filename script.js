5fa9f65076c5d404c31c5167e3b395ce18787dc1
BrendaLDelgado <brenda.l.delgado94@gmail.com>
2024-01-22 21:07:46 -0500

    fixing bugs

--- script.js
+++ script.js
@@ -36,23 +36,23 @@ search.addEventListener('click', () => {
 
             switch (json.weather[0].main) {
                 case 'Clear':
-                    image.src = 'images/clear.png';
+                    image.src = 'weather-icons/clear.png';
                     break;
 
                 case 'Rain':
-                    image.src = 'images/rain.png';
+                    image.src = 'weather-icons/rain.png';
                     break;
 
                 case 'Snow':
-                    image.src = 'images/snow.png';
+                    image.src = 'weather-icons/snow.png';
                     break;
 
                 case 'Clouds':
-                    image.src = 'images/cloud.png';
+                    image.src = 'weather-icons/cloud.png';
                     break;
 
                 case 'Haze':
-                    image.src = 'images/mist.png';
+                    image.src = 'weather-icons/mist.png';
                     break;
 
                 default:
